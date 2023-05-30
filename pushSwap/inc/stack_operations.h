cfc24395604986550539bc42412f5e74aeb9b713
Karol Gebski <kgebski@c2s4d9.42wolfsburg.de>
2023-05-30 17:48:23 +0200

    change gitignore

---
+++ pushSwap/inc/stack_operations.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_operations.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgebski <kgebski@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 17:42:25 by kgebski           #+#    #+#             */
+/*   Updated: 2023/05/30 17:44:18 by kgebski          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_OPERATIONS_H
+# define STACK_OPERATIONS_H
+# include "push_swap.h"
+
+void	operation_sa(t_list **stack_a);
+
+#endif
